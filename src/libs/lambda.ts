80eabb79dfabb0538bcd2f928d1a12cbacd2ae75
Hoang Nguyen <nbhoangit@gmail.com>
2022-05-27 14:10:20 +0700

    update middy v3

--- src/libs/lambda.ts
+++ src/libs/lambda.ts
@@ -1,23 +1,11 @@
 import middy from "@middy/core"
-import ssm from "@middy/ssm"
-import {getInternal} from "@middy/util"
 import inputOutputLogger from '@middy/input-output-logger'
 import errorLogger from '@middy/error-logger'
 import middyJsonBodyParser from "@middy/http-json-body-parser"
 
 export const middyfy = (handler) => {
   return middy(handler)
-    .use(ssm({
-      fetchData: {
-        configs: '/sphtech/swg/dev/swg_check_envs'
-      },
-      cacheKey: 'ssm-configs'
-    }))
     .use(middyJsonBodyParser())
     .use(inputOutputLogger())
     .use(errorLogger())
-    .before(async (request) => {
-      const data = await getInternal(['configs'], request)
-      Object.assign(request.context, data)
-    })
 }
